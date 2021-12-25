0f8f24af086478588d219d2218aa060b5fbc1699
Stanislav <sfedoruk@mail.ru>
2021-12-25 19:09:18 +0600

    commit robots.txt

--- interkom/admin.py
+++ interkom/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from interkom.models import Home, Galvanize, Cuprum, ZinkSetModel
 
-
 admin.site.register(Home)
 admin.site.register(Galvanize)
 admin.site.register(Cuprum)
